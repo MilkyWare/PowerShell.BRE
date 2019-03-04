5957353759602acae77b2373b9337063679e404e
Cameron Cowen <cameronc94@googlemail.com>
2019-03-04 20:47:16 +0000

    Added export rule cmdlet

--- src/PowerShell.BRE/PowerShell.BRE.psm1
+++ src/PowerShell.BRE/PowerShell.BRE.psm1
@@ -1,57 +1,38 @@
 [CmdletBinding()]
-Param ()
-Process {
+param ()
+process {
     $driver = [Microsoft.BizTalk.RuleEngineExtensions.RuleSetDeploymentDriver]::new()
     $ruleStore = $driver.GetRuleStore()
     Write-Verbose "Connected to BRE store on $($ruleStore.Location)"
 
-    function Import-Rule {
+    #region Rules
+    function Export-Rule {
         [CmdletBinding()]
-        Param (
-            [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)]
-            [ValidateScript({$_.Exists})]
-            [System.IO.FileInfo]$Path,
-            [Parameter()]
-            [switch]$Deploy
-        )
-        Process {
-            [xml]$policy = Get-Content -Path $Path.FullName
-            $policy
-    
-            $driver.ImportAndPublishFileRuleStore($Path.FullName)
-            if ($Deploy) {
-                Write-Verbose "Deploying Policy"
-            }
-        }
-    }
-    
-    function Export-Vocabulary {
-        [CmdletBinding()]
-        Param (
-            [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)]
-            [Microsoft.RuleEngine.VocabularyInfo]$Vocabulary,
+        param (
+            [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
+            [Microsoft.RuleEngine.RuleSetInfo]$Rule,
             [Parameter()]
             [string]$Output = "."
         )
-        Process {
-            $fileName = "$($Vocabulary.Name).$($Vocabulary.MajorRevision).$($Vocabulary.MinorRevision).xml"
+        process {
+            $fileName = "$($Rule.Name).$($Rule.MajorRevision).$($Rule.MinorRevision).xml"
             Write-Debug "FileName = $fileName"
             $filePath = Join-Path -Path $Output -ChildPath $fileName
             Write-Debug "FilePath = $filePath"
+            $driver.ExportRuleSetToFileRuleStore($Rule, $filePath)
             Write-Output $filePath
-            $driver.ExportVocabularyToFileRuleStore($Vocabulary, $filePath)
         }
     }
 
     function Get-Rule {
         [CmdletBinding()]
-        Param (
-            [Parameter(Position=0, ValueFromPipeline=$true)]
+        param (
+            [Parameter(Position = 0, ValueFromPipeline = $true)]
             [string]$Name,
             [Parameter()]
             [version]$Version
         )
-        Process {
+        process {
             $rules = if ($PSBoundParameters.ContainsKey("Name")) {
                 $ruleStore.GetRuleSets($Name, [Microsoft.RuleEngine.RuleStore+Filter]::All)
             }
@@ -63,15 +44,55 @@ Process {
         }
     }
 
+    function Import-Rule {
+        [CmdletBinding()]
+        param (
+            [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
+            [ValidateScript( {$_.Exists})]
+            [System.IO.FileInfo]$Path,
+            [Parameter()]
+            [switch]$Deploy
+        )
+        process {
+            [xml]$policy = Get-Content -Path $Path.FullName
+            $policy
+    
+            $driver.ImportAndPublishFileRuleStore($Path.FullName)
+            if ($Deploy) {
+                Write-Verbose "Deploying Policy"
+            }
+        }
+    }
+    #endregion
+    
+    #region Vocabularies
+    function Export-Vocabulary {
+        [CmdletBinding()]
+        param (
+            [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
+            [Microsoft.RuleEngine.VocabularyInfo]$Vocabulary,
+            [Parameter()]
+            [string]$Output = "."
+        )
+        process {
+            $fileName = "$($Vocabulary.Name).$($Vocabulary.MajorRevision).$($Vocabulary.MinorRevision).xml"
+            Write-Debug "FileName = $fileName"
+            $filePath = Join-Path -Path $Output -ChildPath $fileName
+            Write-Debug "FilePath = $filePath"
+            $driver.ExportVocabularyToFileRuleStore($Vocabulary, $filePath)
+            Write-Output $filePath
+        }
+    }
+
     function Get-Vocabulary {
         [CmdletBinding()]
-        Param (
-            [Parameter(Position=0, ValueFromPipeline=$true)]
+        param (
+            [Parameter(Position = 0, ValueFromPipeline = $true)]
             [string]$Name,
             [Parameter()]
             [version]$Version
         )
-        Process {
+        process {
             $vocabs = if ($PSBoundParameters.ContainsKey("Name")) {
                 $ruleStore.GetVocabularies($Name, [Microsoft.RuleEngine.RuleStore+Filter]::All)
             } 
@@ -87,12 +108,12 @@ Process {
     }
 
     function Remove-Vocabulary {
-        [CmdletBinding(SupportsShouldProcess=$true)]
-        Param (
-            [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)]
+        [CmdletBinding(SupportsShouldprocess = $true)]
+        param (
+            [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
             [Microsoft.RuleEngine.VocabularyInfo]$Vocabulary
         )
-        Process {
+        process {
             $dependantRules = $ruleStore.GetDependentRuleSets($Vocabulary)
             if ($dependantRules.Count -gt 0) {
                 Write-Warning "Dependant rules found: $($dependantRules.Count)"
@@ -101,4 +122,5 @@ Process {
             $ruleStore.Remove($Vocabulary)
         }
     }
+    #endregion
 }
